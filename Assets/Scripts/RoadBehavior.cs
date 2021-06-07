d31daf9245ce0306c9b48b0f4389b8a6eaacf958
Kyriakos <kiriakostriadafillidis@gmail.com>
2021-06-07 20:32:03 +0300

    Lecture 7-6-21
    Road Movement added

---
+++ Assets/Scripts/RoadBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadBehavior : MonoBehaviour
+{
+    public float roadOffsetY = 0;
+    public float speed = 1;
+
+    public Material roadMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roadMaterial = gameObject.GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roadOffsetY += speed * Time.deltaTime;
+        roadMaterial.SetTextureOffset("_MainTex", new Vector2(0, -roadOffsetY));
+    }
+}
